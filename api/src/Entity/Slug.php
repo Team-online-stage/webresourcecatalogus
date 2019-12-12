f5cee3ddd770fa838b4cf9c42c92f8561465bd55
Ruben van der Linde <ruben@conduction.nl>
2019-12-12 16:22:02 +0100

    Update on entity description

--- api/src/Entity/Slug.php
+++ api/src/Entity/Slug.php
@@ -2,21 +2,39 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use App\Filter\LikeFilter;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ApiResource()
+ * This is an example entity.
+ * 
+ * @ApiResource(
+ *     normalizationContext={"groups"={"read"}, "enable_max_depth"=true},
+ *     denormalizationContext={"groups"={"write"}, "enable_max_depth"=true},
+ * )
+ * @Gedmo\Loggable
  * @ORM\Entity(repositoryClass="App\Repository\SlugRepository")
  */
 class Slug
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @var \Ramsey\Uuid\UuidInterface
+	 *
+	 * @example e2984465-190a-4562-829e-a8cca81aa35d
+	 *
+	 * @Assert\Uuid
+	 * @Groups({"read"})
+	 * @ORM\Id
+	 * @ORM\Column(type="uuid", unique=true)
+	 * @ORM\GeneratedValue(strategy="CUSTOM")
+	 * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+	 */
+	private $id;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Application")
