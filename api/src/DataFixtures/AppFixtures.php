903d9a2a2d6f1cdc27f75c87fabbd730024904f1
Ruben van der Linde <ruben@conduction.nl>
2020-01-03 22:25:11 +0100

    Added more pages to resource component

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -81,6 +81,120 @@ class AppFixtures extends Fixture
     	$slug->setApplication($application);
     	$slug->setSlug('melding');
     	$manager->persist($page);
+    	    	
+    	// Ceremonie
+    	$template = New Template;
+    	$template->setData('Ceremonie');
+    	$template->setTemplateEngine('twig');
+    	$manager->persist($template);
+    	
+    	$page = New Page;
+    	$page->setTitle('Ceremonie');
+    	$page->setDescription('Ceremonie');
+    	$page->setApplication($application);
+    	$page->setTemplate($template);
+    	$manager->persist($page);
+    	
+    	$slug = New Slug;
+    	$slug->setPage($page);
+    	$slug->setApplication($application);
+    	$slug->setSlug('ceremonie');
+    	$manager->persist($page);
+    	
+    	// Ambtenaren
+    	$template = New Template;
+    	$template->setData('Ambtenaren');
+    	$template->setTemplateEngine('twig');
+    	$manager->persist($template);
+    	
+    	$page = New Page;
+    	$page->setTitle('Ambtenaren');
+    	$page->setDescription('Ambtenaren');
+    	$page->setApplication($application);
+    	$page->setTemplate($template);
+    	$manager->persist($page);
+    	
+    	$slug = New Slug;
+    	$slug->setPage($page);
+    	$slug->setApplication($application);
+    	$slug->setSlug('ambtenaar');
+    	$manager->persist($page);
+    	
+    	// Locatie
+    	$template = New Template;
+    	$template->setData('Locaties');
+    	$template->setTemplateEngine('twig');
+    	$manager->persist($template);
+    	
+    	$page = New Page;
+    	$page->setTitle('Locatie');
+    	$page->setDescription('Locaties');
+    	$page->setApplication($application);
+    	$page->setTemplate($template);
+    	$manager->persist($page);
+    	
+    	$slug = New Slug;
+    	$slug->setPage($page);
+    	$slug->setApplication($application);
+    	$slug->setSlug('locaties');
+    	$manager->persist($page);
+    	
+    	// Extras
+    	$template = New Template;
+    	$template->setData('Extras');
+    	$template->setTemplateEngine('twig');
+    	$manager->persist($template);
+    	
+    	$page = New Page;
+    	$page->setTitle('Extras');
+    	$page->setDescription('Extras');
+    	$page->setApplication($application);
+    	$page->setTemplate($template);
+    	$manager->persist($page);
+    	
+    	$slug = New Slug;
+    	$slug->setPage($page);
+    	$slug->setApplication($application);
+    	$slug->setSlug('extras');
+    	$manager->persist($page);
+    	
+    	// Partner
+    	$template = New Template;
+    	$template->setData('Partner');
+    	$template->setTemplateEngine('twig');
+    	$manager->persist($template);
+    	
+    	$page = New Page;
+    	$page->setTitle('Partner');
+    	$page->setDescription('Partner');
+    	$page->setApplication($application);
+    	$page->setTemplate($template);
+    	$manager->persist($page);
+    	
+    	$slug = New Slug;
+    	$slug->setPage($page);
+    	$slug->setApplication($application);
+    	$slug->setSlug('partner');
+    	$manager->persist($page);
+    	
+    	// Checklist
+    	$template = New Template;
+    	$template->setData('Checklist');
+    	$template->setTemplateEngine('twig');
+    	$manager->persist($template);
+    	
+    	$page = New Page;
+    	$page->setTitle('Checklist');
+    	$page->setDescription('Checklist');
+    	$page->setApplication($application);
+    	$page->setTemplate($template);
+    	$manager->persist($page);
+    	
+    	$slug = New Slug;
+    	$slug->setPage($page);
+    	$slug->setApplication($application);
+    	$slug->setSlug('checklist');
+    	$manager->persist($page);
 
         $manager->flush();
     }
