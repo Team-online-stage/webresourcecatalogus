5f529f71e6f483212fdfbfda72f03dbd213bc051
Ruben van der Linde <ruben@conduction.nl>
2020-01-03 19:02:52 +0100

    error fix

--- api/src/Entity/Application.php
+++ api/src/Entity/Application.php
@@ -5,7 +5,6 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Filter\LikeFilter;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
