970f73df19be31d6fc2d2ccc702761bf4a9199a9
Ruben van der Linde <ruben@conduction.nl>
2020-06-14 20:34:18 +0200

    Activated Pagination

--- api/src/Entity/Template.php
+++ api/src/Entity/Template.php
@@ -23,7 +23,6 @@ use Symfony\Component\Validator\Constraints as Assert;
  * Templates holds information your pages or include in messages.
  *
  * @ApiResource(
- *     attributes={"pagination_items_per_page"=30},
  *     	normalizationContext={"groups"={"read"}, "enable_max_depth"=true},
  *     	denormalizationContext={"groups"={"write"}, "enable_max_depth"=true},
  *     itemOperations={
