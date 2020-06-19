b4b75db31144bcbe3a749fa0cdc32be01bdf82f3
Ruben van der Linde <ruben@conduction.nl>
2020-06-19 12:36:08 +0200

    Aanpassings instemmings mailtje

--- api/src/DataFixtures/HuwelijksplannerFixtures.php
+++ api/src/DataFixtures/HuwelijksplannerFixtures.php
@@ -1424,7 +1424,7 @@ Als je naar de <a href="https://www.huwelijksplanner.online">huwelijksplanner</a
         $template->setName('E-mail instemming');
         $template->setTitle('Instemming voor een huwelijk');
         $template->setDescription('');
-        $template->setContent("Beste {{ contact.givenName }},<br><br>Uw instemming is gevraagd bij een instemmingsverzoek.<br><br><a href='https://irc-ui.huwelijksplanner.online//assents/{{ assent[\"id\"] }}'>Klik hier</a> om op dit verzoek te reageren.<br><br>Met vriendelijke groet,<br><br>Gemeente Utrecht");
+        $template->setContent("Beste {{ contact.givenName }},<br><br>Uw instemming is gevraagd bij een instemmingsverzoek.<br><br><a href='https://irc-ui.huwelijksplanner.online/assents/{{ assent[\"id\"] }}'>Klik hier</a> om op dit verzoek te reageren.<br><br>Met vriendelijke groet,<br><br>Gemeente Utrecht");
         $template->setTemplateEngine('twig');
         $manager->persist($template);
         $template->setId($id);
