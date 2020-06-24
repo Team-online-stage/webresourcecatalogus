48e11934fd770bead5049a86f73e160cce298094
Wilco Louwerse <wilco@louwerse.com>
2020-06-24 13:57:12 +0200

    1line update in fixtures

--- api/src/DataFixtures/WestfrieslandFixtures.php
+++ api/src/DataFixtures/WestfrieslandFixtures.php
@@ -35,6 +35,7 @@ class WestfrieslandFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         if (
+            !$this->params->get('app_build_all_fixtures') &&
             $this->params->get('app_domain') != 'begraven.zaakonline.nl' && strpos($this->params->get('app_domain'), 'begraven.zaakonline.nl') == false &&
             $this->params->get('app_domain') != 'westfriesland.commonground.nu' && strpos($this->params->get('app_domain'), 'westfriesland.commonground.nu') == false
         ) {
