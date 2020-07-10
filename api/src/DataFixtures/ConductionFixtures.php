f22d37ee9c8dffb6cc5cbbc9c04e40238d38faa4
Wilco Louwerse <wilco@conduction.nl>
2020-07-10 10:19:18 +0200

    Update voor tutorials pagina (en openingstijden weggehaald) 2

--- api/src/DataFixtures/ConductionFixtures.php
+++ api/src/DataFixtures/ConductionFixtures.php
@@ -2245,7 +2245,7 @@ class ConductionFixtures extends Fixture
         $template = new Template();
         $template->setName('Tutorials');
         $template->setDescription('Stage Tutorials Page');
-        $template->setContent(file_get_contents(dirname(__FILE__).'/Resources/Conduction/Stage/tutorials.html.twig', 'r'));
+        $template->setContent(file_get_contents(dirname(__FILE__).'/Resources/Conduction/Stage/tutorials/tutorials.html.twig', 'r'));
         $template->setTemplateEngine('twig');
         $manager->persist($template);
         $template->setId($id);
@@ -2261,6 +2261,72 @@ class ConductionFixtures extends Fixture
         $slug->setApplication($stage);
         $slug->setName('tutorials');
         $slug->setSlug('tutorials');
+        $manager->persist($slug);
+
+        $id = Uuid::fromString('4d785bbe-9a17-4c04-a21d-ed9e5a9aeb4b');
+        $template = new Template();
+        $template->setName('Scrum gericht werken en github');
+        $template->setDescription('Dit is een tutorial om je kennis te laten maken met scrum en github');
+        $template->setContent(file_get_contents(dirname(__FILE__).'/Resources/Conduction/Stage/tutorials/scrum-github.html.twig', 'r'));
+        $template->setTemplateEngine('twig');
+        $manager->persist($template);
+        $template->setId($id);
+        $manager->persist($template);
+        $manager->flush();
+        $template = $manager->getRepository('App:Template')->findOneBy(['id'=> $id]);
+        $template->addTemplateGroup($groupPages);
+        $manager->persist($template);
+        $manager->flush();
+
+        $slug = new Slug();
+        $slug->setTemplate($template);
+        $slug->setApplication($stage);
+        $slug->setName('scrum-github');
+        $slug->setSlug('scrum-github');
+        $manager->persist($slug);
+
+        $id = Uuid::fromString('2b08aae3-0fae-4abd-a31f-72c9a72dab13');
+        $template = new Template();
+        $template->setName('Tutorial2');
+        $template->setDescription('Dit is een 2de tutorial.');
+        $template->setContent(file_get_contents(dirname(__FILE__).'/Resources/Conduction/Stage/tutorials/tutorial2.html.twig', 'r'));
+        $template->setTemplateEngine('twig');
+        $manager->persist($template);
+        $template->setId($id);
+        $manager->persist($template);
+        $manager->flush();
+        $template = $manager->getRepository('App:Template')->findOneBy(['id'=> $id]);
+        $template->addTemplateGroup($groupPages);
+        $manager->persist($template);
+        $manager->flush();
+
+        $slug = new Slug();
+        $slug->setTemplate($template);
+        $slug->setApplication($stage);
+        $slug->setName('tutorial2');
+        $slug->setSlug('tutorial2');
+        $manager->persist($slug);
+
+        $id = Uuid::fromString('6264d050-96c7-43cd-93ea-87d421bbf037');
+        $template = new Template();
+        $template->setName('Tutorial3');
+        $template->setDescription('Dit is een 3de tutorial.');
+        $template->setContent(file_get_contents(dirname(__FILE__).'/Resources/Conduction/Stage/tutorials/tutorial3.html.twig', 'r'));
+        $template->setTemplateEngine('twig');
+        $manager->persist($template);
+        $template->setId($id);
+        $manager->persist($template);
+        $manager->flush();
+        $template = $manager->getRepository('App:Template')->findOneBy(['id'=> $id]);
+        $template->addTemplateGroup($groupPages);
+        $manager->persist($template);
+        $manager->flush();
+
+        $slug = new Slug();
+        $slug->setTemplate($template);
+        $slug->setApplication($stage);
+        $slug->setName('tutorial3');
+        $slug->setSlug('tutorial3');
         $manager->persist($slug);
 
         $id = Uuid::fromString('cad4760e-703d-4de6-aefb-1ce11e9ff829');
