6898d0cc20c8fa3e9d9b352a2c2c73164d0bb4b0
Gino <ginokok1996@hotmail.com>
2020-09-15 08:28:03 +0200

    fixes

--- api/src/DataFixtures/StageFixtures.php
+++ api/src/DataFixtures/StageFixtures.php
@@ -126,23 +126,6 @@ class StageFixtures extends Fixture implements DependentFixtureInterface
         $configuration->setOrganization($organization);
         $configuration->setConfiguration(
             [
-<<<<<<< HEAD
-                'mainMenu' => $this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'menus', 'id' => 'fccb7e65-2b56-49a2-8720-724f823f2b00']),
-                'loggedIn' => $this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'menus', 'id' => '58873338-3ef1-4764-a1a8-72a8787625f4']),
-                'home' => $this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'templates', 'id' => '0e3ec00f-c17b-4237-b6dd-070f800eb784']),
-                'studenten' => $this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'templates', 'id' => 'a4a9a984-d83e-44ac-b27d-c77cd74b0d21']),
-                'footer1' => $this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'templates', 'id' => '3895915c-a992-462e-848d-3be73a954d51']),
-                'footer4' => $this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'templates', 'id' => '0c663ab8-f9d5-42c5-8866-1a51fcf74a12']),
-                'headerimg' => $this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'images', 'id' => 'da8af35b-afca-455e-a722-6d0052f7367d']),
-                'kladimg' => $this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'images', 'id' => '62685881-e5a2-4f73-b08f-a155b6dab74c']),
-                'raketimg' => $this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'images', 'id' => 'cdaad46c-f1b3-11ea-adc1-0242ac120002']),
-                'footer4img' => $this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'images', 'id' => 'e49586fb-ec10-4f92-8ad5-f78e323ac104']),
-                'googleTagId' => 'G-2PYCJ13YC4',
-                'userPage' => 'me',
-                'login' => ['facebook' => true, 'github' => true],
-                'header' => true,
-                'stickyMenu' => true,
-=======
                 'mainMenu'              => $this->commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'menus', 'id'=>'fccb7e65-2b56-49a2-8720-724f823f2b00']),
                 'loggedIn'              => $this->commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'menus', 'id'=>'58873338-3ef1-4764-a1a8-72a8787625f4']),
                 'home'                  => $this->commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'templates', 'id'=>'d6127f56-c334-4eb7-bade-c70e97631aec']),
@@ -157,7 +140,6 @@ class StageFixtures extends Fixture implements DependentFixtureInterface
                 'login'                 => ['facebook'=>true, 'github'=>true],
                 'header'                => true,
                 'stickyMenu'            => true,
->>>>>>> development
             ]
         );
         $manager->persist($configuration);
